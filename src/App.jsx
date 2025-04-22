eb2ef95b8766cb8834b432d713466316b805fb8a
Neontus <junokimzone@gmail.com>
2025-04-22 12:42:10 -0400

    added boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Amazon Review Summarizer</h1>
+      <p>Click the button to summarize reviews.</p>
+      <button>Summarize</button>
+    </div>
+  );
+};
+
+export default App;
